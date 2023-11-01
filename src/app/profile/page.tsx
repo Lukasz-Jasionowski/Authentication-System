50f16a974666d21cf6d13e9ab7f9e7a12c271611
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-11-01 21:14:16 +0100

    Update ProfilePage with user data display and data fetching

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,9 +2,11 @@
 import axios from 'axios';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 export default function ProfilePage() {
     const router = useRouter();
+    const [data, setData] = useState('');
     const logout = async () => {
         try {
             await axios.get('/api/users/logout');
@@ -15,15 +17,25 @@ export default function ProfilePage() {
         }
     }
 
+    const getUserDetails = async () => {
+        const res = await axios.get('/api/users/me');
+        console.log(res.data);
+        setData(res.data.data._id);
+    }
+
     return (
         <div className='flex flex-col items-center justify-center min-h-screen py-2'>
             <h1>Profile</h1>
             <hr />
             <p>Profile page</p>
+            <h2 className='p-2 rounded bg-green-900'>{data === '' ? 'Nothing' : <Link href={`/profile/${data}`}>{data}</Link>}</h2>
             <hr />
             <button
                 onClick={logout}
                 className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded'>Logout</button>
-        </div>
+            <button
+                onClick={getUserDetails}
+                className='bg-green-800 hover:bg-green-600 text-white font-bold py-2 px-4 mt-4 rounded'>Get User Details</button>
+        </div >
     )
 }
