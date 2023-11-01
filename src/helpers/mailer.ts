4617208adf7fc81374fd3a2b7cbc5011fcbbeb83
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-11-01 22:26:24 +0100

    Create email data with token hashing and user data

---
+++ src/helpers/mailer.ts
@@ -0,0 +1,38 @@
+import nodemailer from 'nodemailer';
+import User from '@/models/userModel';
+import bcryptjs from 'bcryptjs';
+
+export const sendEmail = async ({ email, emailType, userId }: any) => {
+    try {
+        //Create hashed token
+        const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+
+        if (emailType === 'VERIFY') {
+            await User.findByIdAndUpdate(userId, { verifyToken: hashedToken, verifyTokenExpiry: Date.now() + 3600000 });
+
+        } else if (emailType === 'RESET') {
+            await User.findByIdAndUpdate(userId, { forgotPasswordToken: hashedToken, forgotPasswordTokenExpiry: Date.now() + 3600000 });
+        }
+
+        var transport = nodemailer.createTransport({
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+                user: process.env.NODEMAILER_USER,
+                pass: process.env.NODEMAILER_PASS
+            }
+        });
+
+        const mailOptions = {
+            from: 'json@gmail.com',
+            to: email,
+            subject: emailType === 'VERIFY' ? 'Verify your email' : 'Reset your password',
+            html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">Here</a> to ${emailType === 'VERIFY' ? 'verify your email' : 'reset your password'} < /p>`
+        }
+
+        //TODO: sendMail
+
+    } catch (error: any) {
+        throw new Error(error.message);
+    }
+}
