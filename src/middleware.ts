8a7b197bd9b5603de2ee681e3dbc465a011ef6f9
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-11-02 22:19:42 +0100

    Update middleware for new public path '/verifyemail'

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname;
-    const isPublicPath = path === '/login' || path === '/signup';
+    const isPublicPath = path === '/login' || path === '/signup' || path === '/verifyemail';
     const token = request.cookies.get('token')?.value || '';
     if (isPublicPath && token) {
         return NextResponse.redirect(new URL('/', request.nextUrl));
@@ -18,5 +18,6 @@ export const config = {
         '/profile',
         '/login',
         '/signup',
+        '/verifyemail',
     ]
 }
